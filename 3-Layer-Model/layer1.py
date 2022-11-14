0cd3987789e8a6587799bde91d3b7eab99dcfa95
Shivam Sagar <technoshivam12@gmail.com>
2022-11-14 18:47:28 +0530

    Kmeans-1-iter

--- 3-Layer-Model/layer1.py
+++ 3-Layer-Model/layer1.py
@@ -3,15 +3,18 @@ import math
 from sklearn_extra.cluster import KMedoids
 from DataAcquisition import *
 from layer2 import *
+from OptLoc import optimalLocationFinder, singleIteration
 
 # optTrans = KMedoids(n_clusters=6).fit(cand_latlon)
 # print(optTrans.cluster_centers_)
 connectionPercentage=0
 nmax = len(cand_latlon)
 print("Cand loc size",nmax)
+
 n=1
 while connectionPercentage<100 and n < nmax:
-    optTrans = KMedoids(n_clusters=n).fit(cand_latlon)
+    optTrans = optimalLocationFinder(n);
+    # optTrans = singleIteration(n);
     # print(optTrans.cluster_centers_)
     connectionPercentage = secondlayer(optTrans, resi_lat, resi_lon)
     print("K = ",n)
@@ -20,3 +23,7 @@ while connectionPercentage<100 and n < nmax:
 
 # print("SUCCESSFULLY CONNECTED...")
 # print("Connection percentage: ", connectionPercentage)
+print("53,54,55: ")
+print(resi_lat[52]," ",resi_lon[52])
+print(resi_lat[53]," ",resi_lon[53])
+print(resi_lat[54]," ",resi_lon[54])
